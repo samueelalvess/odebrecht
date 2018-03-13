de3de3abff08c81ef20908b7820e9db71ae7e82b
Mayron Raifur <mayron@w8tecnologia.com.br>
2018-03-13 20:06:17 -0300

    dados cliente lista

--- app/Model/Client.php
+++ app/Model/Client.php
@@ -11,15 +11,14 @@ class Client extends Model
 
     public function getClientAll()
     {
-      return $this->select('A03_001_C','A03_002_C','A03_003_C','A03_004_C','A03_005_C','A03_006_C','A03_008_M',
-                           'A03_017_C','A03_012_C','A03_014_C','A03_015_C','A03_016_C','A03_009_C','A03_010_C','A24_001_C',
-                           'A33_UKEY')
+      return $this->select('A03_001_C','A03_002_C','A03_003_C','A03_012_C','A03_009_C','A03_010_C','A24_001_C','A03_017_C')
                   ->leftJoin('A24', 'A03.A24_UKEY', '=', 'A24.UKEY')
                   ->Where('ARRAY_003','1')
                   ->Where('A33_UKEY','JAHMO0UMYK_2HE15BBI8')
-                  ->limit(50)  // chave do vendedor!!
+                  ->limit()  // chave do vendedor!!
                   ->get();
     }
+
     public function getClientId($id)
     {
       return $this->select('A03_001_C','A03_002_C','A03_003_C','A03_004_C','A03_005_C','A03_006_C','A03_008_M',
@@ -30,4 +29,5 @@ class Client extends Model
                   ->Where('A33_UKEY','JAHMO0UMYK_2HE15BBI8')  // chave do vendedor!!
                   ->get();
     }
+
 }
