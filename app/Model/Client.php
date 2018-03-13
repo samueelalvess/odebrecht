93e049cdfdd72f7674832a1197b66f3cd76316aa
Mayron Raifur <mayron@w8tecnologia.com.br>
2018-03-13 16:42:37 -0300

    cidade cliente3

--- app/Model/Client.php
+++ app/Model/Client.php
@@ -14,7 +14,6 @@ class Client extends Model
       return $this->select('A03_001_C','A03_002_C','A03_003_C','A03_004_C','A03_005_C','A03_006_C','A03_008_M',
                            'A03_017_C','A03_012_C','A03_014_C','A03_015_C','A03_016_C','A03_009_C','A03_010_C','A24_001_C',
                            'A33_UKEY')
-                           
                   ->leftJoin('A24', 'A03.A24_UKEY', '=', 'A24.UKEY')
                   ->Where('ARRAY_003','1')
                   ->Where('A33_UKEY','JAHMO0UMYK_2HE15BBI8')
