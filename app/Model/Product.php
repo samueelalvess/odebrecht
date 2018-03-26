50c8d4cb95d441036c6a242cb668596ac5a6ecd1
Samuel <samuel@w8tecnologia.com.br>
2018-03-26 16:47:13 -0300

    teste merge

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
 
     public function getProductAll()
     {
-      $data = $this->select('D04_001_C','D04_002_C') 
+      $data = $this->select('D04_001_C','D04_002_C')
                    ->leftJoin('D17','D04.D17_UKEY','=','D17.UKEY')
                    ->leftJoin('D03','D04.D03_UKEY','=','D03.UKEY')
                    ->where('D04.D04_039_N',0)
@@ -19,8 +19,8 @@ class Product extends Model
 
                                   ->orWhere(function ($query2) {
                                       $query2->where('D04.D04_077_N',1)
-                                      $query2->where('D03.D03_004_N',1)
-                                  });
+                                             ->where('D03.D03_004_N',1)
+                                  })
                             })
                    ->orderBy('D04_001_C','desc')
                    ->get();
