03d9175c1c6a7189be4023456b398a171a0eb35c
Mayron Raifur <mayron@w8tecnologia.com.br>
2018-04-03 10:20:49 -0300

    aplicando selects joab vendedor e cliente

--- app/Model/Client.php
+++ app/Model/Client.php
@@ -15,7 +15,7 @@ class Client extends Model
       return $this->select('A03_001_C','A03_002_C','A03_003_C','A03_012_C','A03_009_C','A03_010_C','A24_001_C','A03_017_C','A03_500_C')
                   ->leftJoin('A24', 'A03.A24_UKEY', '=', 'A24.UKEY')
                   ->Where('ARRAY_003','1')
-                  ->Where('A33_UKEY','JAHMO0UMYK_2HE15BBI8')
+                  ->Where('A33_UKEY','7CB0A0344D4C414E95CC')
                   ->get();
     }
 
--- app/Model/Seller.php
+++ app/Model/Seller.php
@@ -10,12 +10,15 @@ class Seller extends Model
 
   public function getSellerAll()
   {
-    $data = $this->select('A33_001_C','A33_002_C','A33_003_C','A33_010_C','A33_012_C','A33_024_C','ARRAY_009','A24_001_C')
+    $data = $this->select('A33_001_C','A33_002_C','A33_003_C','A33_010_C','A33_012_C','A33_024_C','A33.ARRAY_009','A24_001_C')
                  ->leftJoin('A24', 'A33.A24_UKEY', '=', 'A24.UKEY')
-                 ->where('ARRAY_003','1')
+                 ->Join('A03','A33.A03_UKEY', '=', 'A03.UKEY')
+                 ->where('A33.ARRAY_003','1')
+                 ->where('A03_500_c','<>','')
                  ->get();
     return $data;
   }
+
   public function getSellerId($id)
   {
     return $this->select()
