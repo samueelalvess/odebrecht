234f61325217fbf2d21fded6ed903f6cf15a8481
Mayron Raifur <mayron@w8tecnologia.com.br>
2018-04-03 09:24:16 -0300

    model product editar

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -14,14 +14,14 @@ class Product extends Model
                    ->leftJoin('D17','D04.D17_UKEY','=','D17.UKEY')
                    ->leftJoin('D03','D04.D03_UKEY','=','D03.UKEY')
                    ->where('D04.D04_039_N',0)
-                   ->where(function ($query) {
-                                $query->where('D04.D04_077_N',2)
+                   //where(function ($query) {
+                    //            $query->where('D04.D04_077_N',2)
 
-                                  ->orWhere(function ($query2) {
-                                      $query2->where('D04.D04_077_N',1)
-                                              ->where('D03.D03_004_N',1);
-                                  });
-                            })
+                      //            ->orWhere(function ($query2) {
+                        //              $query2->where('D04.D04_077_N',1)
+                          //                    ->where('D03.D03_004_N',1);
+                            //      });
+                            //})
                    ->orderBy('D04_001_C','desc')
                    ->get();
 
