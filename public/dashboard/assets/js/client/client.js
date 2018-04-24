a45fc6d00cf98bf6269734dd26eb80c7636a7eb0
Samuel <samuel@w8tecnologia.com.br>
2018-04-24 10:15:00 -0300

    tela cliente selecionar cliente para iniciar pedido

--- public/dashboard/assets/js/client/client.js
+++ public/dashboard/assets/js/client/client.js
@@ -31,3 +31,14 @@ $(document).ready(function() {
   );
 
 } );
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function setCliente(cliente) {
+  setCookie('cliente',cliente,'1');
+}
