9adb91ca3fce71d9d334a779b5e950741bd315f7
Mayron Raifur <mayron@w8tecnologia.com.br>
2018-05-02 15:30:46 -0300

    impressao

--- resources/views/dashboard/seller/linkseller.blade.php
+++ resources/views/dashboard/seller/linkseller.blade.php
@@ -25,21 +25,13 @@
 
         <div class="col-md-12 col-sm-6 col-xs-12 form-group has-feedback">
           <label>Nome Vendedor</label>
-<<<<<<< HEAD
           <input type="text" name="name" required class="form-control has-feedback-left" value="@if(!empty($seller->id)){{trim($seller->name)}} @else {{trim($seller->A33_002_C)}} @endif">
-=======
-          <input type="text" name="name" required class="form-control has-feedback-left" value="@if(!empty($seller)){{trim($seller->A33_002_C)}}@endif @if(!empty($user)){{trim($user->name)}}@endif">
->>>>>>> 0c05d258a77fcc8e0b2ff36fba02e013f53e287c
           <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
         </div>
 
         <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
           <label>Email</label>
-<<<<<<< HEAD
           <input type="text" required name="email" class="form-control has-feedback-left" value="@if(!empty($seller->id)) {{trim($seller->email)}} @else {{trim($seller->A33_024_C)}} @endif" placeholder="Email do Vendedor">
-=======
-          <input type="text" required name="email" class="form-control has-feedback-left" value="@if(!empty($seller)){{trim($seller->A33_024_C)}}@endif @if(!empty($user)){{trim($user->email)}}@endif" placeholder="Email do Vendedor">
->>>>>>> 0c05d258a77fcc8e0b2ff36fba02e013f53e287c
           <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
         </div>
 
